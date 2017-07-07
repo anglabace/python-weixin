39b7bc4165b394b41ce56059356094379e4a9eb4
Liu Zhiwang <liuzhiwang@xunlei.com>
2017-07-07 10:21:54 +0800

    first commit

---
+++ weixin.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+import time
+import json
+from pprint import pprint
+
+post = {}
+
+driver = webdriver.Chrome('./chromedriver')
+driver.get('https://mp.weixin.qq.com/')
+time.sleep(2)
+driver.find_element_by_xpath("./*//input[@id='account']").clear()
+driver.find_element_by_xpath("./*//input[@id='account']").send_keys('liuzhiwang4480@gmail.com')
+driver.find_element_by_xpath("./*//input[@id='pwd']").clear()
+driver.find_element_by_xpath("./*//input[@id='pwd']").send_keys('Liu741369852')
+
+time.sleep(5)
+driver.find_element_by_xpath("./*//a[@id='loginBt']").click()
+
+time.sleep(15)
+# driver.get('https://mp.weixin.qq.com/')
+cookie_items = driver.get_cookies()
+for cookie_item in cookie_items:
+    post[cookie_item['name']] = cookie_item['value']
+cookie_str = json.dumps(post)
+with open('cookie.txt', 'w+') as f:
+    f.write(cookie_str)
